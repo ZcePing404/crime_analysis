b486a072e6ccf84b4bb074a75d293584d88cb903
ZcePing404 <zceping@gmail.com>
2025-08-01 16:30:26 +0800

    Added outlier remove part

--- correlation_coefficient.py
+++ correlation_coefficient.py
@@ -10,23 +10,10 @@ target_col = 'ViolentCrimesPerPop'
 # Calculate correlation with target (excluding the target itself)
 correlation_matrix = clean_df.corr()[target_col].drop(target_col)
 
-# Filter the correlation which above +-0.45
-# strong_corr = correlation_matrix[target_col][correlation_matrix[target_col].abs() > 0.45]
-# strong_corr_features = strong_corr.index.tolist()
-
-# # Include only strong correlated features
-# filtered_corr_matrix = clean_df[strong_corr_features].corr()
-
-# Plot
-# plt.figure(figsize=(10, 8))
-# sns.heatmap(filtered_corr_matrix, cmap='coolwarm', annot=True, fmt=".2f", linewidths=0.5)
-# plt.title("Correlation Matrix of Strongly Correlated Features")
-# plt.tight_layout()
-# Unstack and reset index
-
-
+filtered_correlation = correlation_matrix[correlation_matrix.abs() > 0.25]
 # Convert to DataFrame and sort
-correlation_df = correlation_matrix.sort_values()
+correlation_df = filtered_correlation.sort_values()
+print(correlation_df.shape[0])
 
 # Plot
 plt.figure(figsize=(20, 6))  # WIDE plot to fit all 128 attribute names
--- outlier_detect.py
+++ outlier_detect.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from ucimlrepo import fetch_ucirepo
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def remove_unuseful(df):
     # Drop columns with non-predictable columns
@@ -21,6 +23,20 @@ def handle_missing(df):
     df = df.fillna(df.mean())
     return df
 
+def remove_outliers_iqr(df):
+    Q1 = df.quantile(0.25)
+    Q3 = df.quantile(0.75)
+    IQR = Q3 - Q1
+    iqr_df = pd.DataFrame({
+        'Q1 (25%)': Q1,
+        'Q3 (75%)': Q3,
+        'IQR': IQR
+    })
+
+    print(iqr_df)
+    # Keep rows where all columns are within the IQR range
+    mask = ~((df < (Q1 - 1.5 * IQR)) | (df > (Q3 + 1.5 * IQR))).any(axis=1)
+    return df[mask]
 
 def remove_multi_collinearity(df):
     abs_corr_matrix = df.corr().abs()  # Take absolute correlation
@@ -30,15 +46,32 @@ def remove_multi_collinearity(df):
         np.triu(np.ones(abs_corr_matrix.shape), k=1).astype(bool)
     )
     # Find columns with high correlation (e.g., > 0.9)
-    to_drop = [column for column in upper.columns if any(upper[column] > 0.9)]
+    to_drop = [column for column in upper.columns if any(upper[column] > 0.8)]
 
     df = df.drop(columns=to_drop)
     return df
+
+def plot_boxplots_before_after(df_before, df_after, features, ncols=3):
+    n = len(features)
+    nrows = (n + ncols - 1) // ncols
+
+    plt.figure(figsize=(ncols*5, nrows*4))
+
+    for i, col in enumerate(features):
+        plt.subplot(nrows, ncols, i+1)
+        sns.boxplot(data=pd.concat([df_before[col], df_after[col]], axis=1),palette='Set2')
+        plt.xticks([0, 1], ['Before', 'After'])
+        plt.title(col)
+
+    plt.tight_layout()
+    plt.show()
 
 
 # Fetch dataset
 communities_dataset = fetch_ucirepo(id=183)
 df = pd.DataFrame(communities_dataset.data.original)
+df_before_outlier_removal = df.copy()
+features_to_plot = ['population', 'householdsize', 'racepctblack', 'racePctAsian', 'racePctHisp']
 
 print(f"Initial number of attributes: {df.shape[1]}")
 
@@ -54,15 +87,30 @@ df = handle_missing(df)
 after = df.shape[1]
 print(f"After handle missing values     : {after} attributes (Removed {before - after})")
 
-# Step 3: Remove remove multi-collinearity attiribute
+# Step 4: Remove remove multi-collinearity attiribute
 before = df.shape[1]
 df = remove_multi_collinearity(df)
 after = df.shape[1]
 print(f"After remove redundant features : {after} attributes (Removed {before - after})")
 
+# Step 3: Remove outliers
+before_rows = df.shape[0]
+df = remove_outliers_iqr(df)
+after_rows = df.shape[0]
+print(f"After removing outliers         : {after_rows} rows (Removed {before_rows - after_rows})")
+
 print(f"\nFinal number of features      : {after}")
 print(df.columns.tolist())
 
-df.to_csv('./dataset/clean_dataset.csv')
+# df.to_csv('./dataset/clean_dataset_without_outlier.csv')
+
+sns.boxplot(data=df_before_outlier_removal['population'])
+plt.title('Before Removing Outliers')
+plt.show()
+
+# After removing outliers
+sns.boxplot(data=df['population'])
+plt.title('After Removing Outliers')
+plt.show()
 
 
